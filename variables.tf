022810c4435046e1ef68527f843b4a00df0aaf17
Muthu-hari <66533153+Muthu-hari@users.noreply.github.com>
2020-09-20 20:16:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
+variable "aws_access_key" {
+  type = string
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  type = string
+  description = "AWS secret key"
+}
+
+variable "aws_region" {
+  type = string
+  description = "AWS region"
+}
+
 variable "instance_count" {
   description = "Number of instances to launch"
   type        = number
