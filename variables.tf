e8f916808286cbddb32f6d22bedbe6217ebe49db
Muthu-hari <66533153+Muthu-hari@users.noreply.github.com>
2020-09-20 19:40:38 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "instance_count" {
+  description = "Number of instances to launch"
+  type        = number
+  default     = 3
+}
+
+variable "ami" {
+  description = "ID of AMI to use for the instance"
+  type        = string
+  default     = "ami-ebd02392"
+}
+
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default     = "t2.micro"
+}
